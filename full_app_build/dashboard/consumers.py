053a493e96597885255ffd6269d3d0e39b9d5981
joselynr <joselynr@student.21-school.ru>
2024-06-15 15:50:56 +0700

    update

--- full_app_build/dashboard/consumers.py
+++ full_app_build/dashboard/consumers.py
@@ -6,6 +6,7 @@ from threading import Thread
 import pandas as pd
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from django.db.models.functions import Lower
 from channels.exceptions import DenyConnection
 from .models import DetailAmry, MyPrice
 
@@ -42,7 +43,7 @@ class DetailConsumer(AsyncJsonWebsocketConsumer):
 
     async def disconnect(self, close_code):
         self.parse = False
-        MyPrice.objects.all().delete()
+        # await MyPrice.objects.all().adelete()
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -74,10 +75,10 @@ class DetailConsumer(AsyncJsonWebsocketConsumer):
         )
 
     def simulate(self):
-        base_thread = Thread(target=self.database_prices)
-        base_thread.start()
-        # amry_thread = Thread(target=self.start_amry, args=('amry',))
-        # amry_thread.start()
+        # base_thread = Thread(target=self.database_prices)
+        # base_thread.start()
+        amry_thread = Thread(target=self.start_amry, args=('amry',))
+        amry_thread.start()
         armtek_thread = Thread(target=self.start_amry, args=('armtek',))
         armtek_thread.start()
         carreta_thread = Thread(target=self.start_amry, args=('carreta',))
@@ -85,17 +86,14 @@ class DetailConsumer(AsyncJsonWebsocketConsumer):
         emex_thread = Thread(target=self.start_amry, args=('emex',))
         emex_thread.start()
 
-        # percent = 0.5
-        # while True:
-        #     prices = MyPrice.objects.filter(send=False)
-        #     for price in prices:
-        #         price.amry = random.randint(int(price.buyPrice - price.buyPrice * percent), int(price.buyPrice + price.buyPrice * percent))
-        #         price.armtek = random.randint(int(price.buyPrice - price.buyPrice * percent), int(price.buyPrice + price.buyPrice * percent))
-        #         price.carreta = random.randint(int(price.buyPrice - price.buyPrice * percent), int(price.buyPrice + price.buyPrice * percent))
-        #         price.emex = random.randint(int(price.buyPrice - price.buyPrice * percent), int(price.buyPrice + price.buyPrice * percent))
-        #         price.save()
-        #         time.sleep(0.4)
-      
+        amry_thread.join()
+        armtek_thread.join()
+        carreta_thread.join()
+        emex_thread.join()
+
+        self.send_rows()
+
+
     def start_amry(self, site):
         if site == "amry":
             self.get_price_amry()
@@ -107,41 +105,54 @@ class DetailConsumer(AsyncJsonWebsocketConsumer):
             asyncio.run(emex_api.main())
 
     def get_price_amry(self):
-        while True:
+        index = 0
+        all_prices = len(MyPrice.objects.all())
+        while index < all_prices:
             prices = MyPrice.objects.filter(send=False, amry=0)
             for row in prices:
-                detail = DetailAmry.objects.filter(article = row.article).order_by('-price').first()
+                brand = row.brand.strip().lower()
+                detail = DetailAmry.objects.filter(article = row.article, brand__iexact = brand, quantity__gt = 1).order_by('-price').first()
                 if not detail is None:
                     MyPrice.objects.filter(article = row.pk).update(amry = detail.price)
-                    # row.amry = detail.price
-                    # row.save()
+                
+                index += 1
 
     def database_prices(self):
-        while self.i < 60:
-            prices = MyPrice.objects.filter(send=False)
-            for price in prices:
-                if self.i < 60 and price.emex != 0 and price.carreta != 0 and price.armtek != 0:
-                    row = {
-                        'detail' : price.detail,
-                        'article' : price.article,
-                        'price' : price.buyPrice,
-                        'carreta' : price.carreta,
-                        'amry' : price.amry,
-                        'armtek' : price.armtek,
-                        'emex' : price.emex
-                    }
-                    time.sleep(0.1)
-                    asyncio.run(self.send(json.dumps(row)))
-                    price.send = True
-                    price.page = self.page
-                    price.save()
-                    self.i += 1
-                else: 
-                    continue
-        self.max_page = self.page
+        all_prices = MyPrice.objects.all()
+        if (self.page != 1 and len(all_prices) >= (self.page - 1) * 50) or self.page == 1:
+            while self.i < 50:
+                prices = MyPrice.objects.filter(send=False)
+                for price in prices:
+                    if self.i < 50 and price.emex != 0 and price.carreta != 0 and price.armtek != 0:
+                        row = {
+                            'detail' : price.detail,
+                            'article' : price.article,
+                            'price' : price.buyPrice,
+                            'carreta' : price.carreta,
+                            'amry' : price.amry,
+                            'armtek' : price.armtek,
+                            'emex' : price.emex
+                        }
+                        time.sleep(0.1)
+                        asyncio.run(self.send(json.dumps(row)))
+                        price.send = True
+                        price.page = self.page
+                        price.save()
+                        self.i += 1
+                    else: 
+                        continue
+            self.max_page = self.page
+        else:
+            return
         
     def database_price_page(self):
-        prices = MyPrice.objects.filter(page=self.page)
+        check_page = True
+        while check_page:
+            prices = MyPrice.objects.filter(page=self.page)
+            if len(prices) == 0:
+                self.page -= 1
+            else:
+                check_page = False
         for price in prices:
             row = {
                 'detail' : price.detail,
@@ -153,3 +164,27 @@ class DetailConsumer(AsyncJsonWebsocketConsumer):
                 'emex' : price.emex
             }
             asyncio.run(self.send(json.dumps(row)))
+
+    def send_rows(self):
+        prices = MyPrice.objects.filter(send=False)
+        for price in prices:
+            if self.i < 50:
+                row = {
+                    'detail' : price.detail,
+                    'article' : price.article,
+                    'price' : price.buyPrice,
+                    'carreta' : price.carreta,
+                    'amry' : price.amry,
+                    'armtek' : price.armtek,
+                    'emex' : price.emex
+                }
+                time.sleep(0.1)
+                asyncio.run(self.send(json.dumps(row)))
+                price.send = True
+                price.page = self.page
+                price.save()
+                self.i += 1
+            else:
+                return
+            
+        return
